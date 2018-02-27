503c214b255ba09db61fa4bade073e2c4a4864d6
Fernando Iazeolla <fernando.iazeolla@gmail.com>
2018-02-27 14:52:46 +0100

    pyquery-cli

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pyquery-cli',
+    version='0.1',
+    scripts=['pyquery-cli'],
+    author='Fernando Iazeolla',
+    description='A command line interface for JQuery like search and extract data from HTML.',
+    keywords=['jquery', 'pyquery', 'html'],
+    install_requires=['pyquery'],
+    license='GPLv2+',
+    url=''
+)
