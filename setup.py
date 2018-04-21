ccebb550eff55d4e3af44c9ff31cc073e050c12b
Fernando Iazeolla <fernando.iazeolla@gmail.com>
2018-04-21 20:12:49 +0200

    added cfg file, license and other metadata.

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ setup(
     version='0.0.1',
     scripts=['pyquery-cli'],
     author='Fernando Iazeolla',
+    uthor_email='xnando@yahoo.it',
     description='A command line interface for JQuery like search and extract data from HTML. see the README.md file.',
     keywords=['jquery', 'pyquery', 'html'],
     install_requires=['pyquery'],
-    license='GPLv2+',
-    url='',
+    license='L-Beerware',
+    url='https://github.com/elboza/PyQuery',
+    download_url='https://github.com/elboza/PyQuery/archive/0.0.1.tar.gz',
     classifiers=["Programming Language :: Python", "Programming Language :: Python :: 3", "Operating System :: OS Independent"]
 )
